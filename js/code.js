8c19c5aa863d2082a77b0a163cdd0ec364d49ec1
wsm <1030057982@qq.com>
2019-10-23 20:02:52 +0800

    simple web

---
+++ js/code.js
@@ -0,0 +1,21 @@
+!(function () {
+	let cssCode = window.cssCode = `
+/*你好，我是天津工业大学一名在读学生。
+请允许我对我们学校做一个简单介绍，纯文字太单调，我想来点特别的。
+首先我准备一下样式。*/
+*{
+	transition: all .5s;
+}
+/*白色伤眼睛，来点暗色系的背景吧！*/
+html{
+	background: #999;
+}
+/*让我们给它加一个边框吧*/
+#code{
+    border: 2px solid #00FF1B;
+    padding: 20px;
+}
+/*现在开始写介绍吧*/
+/*首先我需要一张纸*/
+`;
+})()
